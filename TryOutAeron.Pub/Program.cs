6370199d51914c0a977a4842488c93aa93215c94
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2025-05-09 06:59:14 +0100

    poc for IPC

---
+++ TryOutAeron.Pub/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using Adaptive.Aeron;
+using Adaptive.Agrona.Concurrent;
+
+const string channel = "aeron:ipc";
+const int streamId = 42;
+
+var buffer = new UnsafeBuffer(new byte[256]);
+
+using var aeron = Aeron.Connect();
+using var publisher = aeron.AddPublication(channel, streamId);
+
+var message = buffer.PutStringWithoutLengthUtf8(0, "Hello World!");
+publisher.Offer(buffer, 0, message);
+Console.WriteLine("Sent message");
---
+++ TryOutAeron.Sub/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using System.Threading;
+using Adaptive.Aeron;
+using Adaptive.Aeron.LogBuffer;
+using Adaptive.Agrona;
+using Adaptive.Agrona.Concurrent;
+
+const string channel = "aeron:ipc";
+const int streamId = 42;
+
+var handler = HandlerHelper.ToFragmentHandler(PrintMessage);
+
+try {
+    using var aeron = Aeron.Connect();
+    using var subscriber = aeron.AddSubscription(channel, streamId);
+
+    while (subscriber.Poll(handler, 1) == 0) {
+        Thread.Sleep(10);
+    }
+}
+catch (Exception e) {
+    Console.WriteLine(e);
+}
+finally {
+    Console.WriteLine("Press any key to continue");
+    Console.ReadKey();
+}
+return;
+
+void PrintMessage(IDirectBuffer buffer, int offset, int length, Header header)
+{
+    var message = buffer.GetStringWithoutLengthUtf8(offset, length);
+    Console.WriteLine($"Received message ({message}) to stream {header.StreamId:D} from session {header.SessionId:x} term id {header.TermId:x} term offset {header.TermOffset:D} ({length:D}@{offset:D})");
+}

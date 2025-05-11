4c10e31d27353f349e0fa45bf3d34c4164c0299d
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2025-05-11 17:24:12 +0100

    add some extensions for asynchronous message publication

---
+++ TryOutAeron.Pub/Extensions/PublicationExtensions.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Threading.Tasks;
+using Adaptive.Aeron;
+using Adaptive.Agrona;
+using Adaptive.Agrona.Concurrent;
+
+namespace TryOutAeron.Pub.Extensions;
+
+public static class PublicationExtensions
+{
+    private static async ValueTask OfferAsync(Func<long> offerAction)
+    {
+        while (true) {
+            var result = offerAction();
+            if (result >= 0) return;
+            if (result == Publication.BACK_PRESSURED) {
+                await Task.Delay(10);
+                continue;
+            }
+            throw new InvalidOperationException($"Offer failed with result {result}");
+        }
+    }
+
+    public static ValueTask OfferAsync(
+        this Publication publication,
+        UnsafeBuffer buffer
+    ) => OfferAsync(() => publication.Offer(buffer));
+
+    public static ValueTask OfferAsync(
+        this Publication publication,
+        IDirectBuffer buffer,
+        int offset,
+        int length,
+        ReservedValueSupplier? reservedValueSupplier = null
+    ) => OfferAsync(() => publication.Offer(buffer, offset, length, reservedValueSupplier));
+
+    public static ValueTask OfferAsync(
+        this Publication publication,
+        IDirectBuffer bufferOne,
+        int offsetOne,
+        int lengthOne,
+        IDirectBuffer bufferTwo,
+        int offsetTwo,
+        int lengthTwo,
+        ReservedValueSupplier? reservedValueSupplier = null
+    ) => OfferAsync(() => publication.Offer(
+        bufferOne,
+        offsetOne,
+        lengthOne,
+        bufferTwo,
+        offsetTwo,
+        lengthTwo,
+        reservedValueSupplier
+    ));
+
+    public static ValueTask OfferAsync(
+        this Publication publication,
+        DirectBufferVector[] vectors,
+        ReservedValueSupplier? reservedValueSupplier = null
+    ) => OfferAsync(() => publication.Offer(vectors, reservedValueSupplier));
+}

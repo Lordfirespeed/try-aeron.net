b5ec8c5433f2d226953c4ffdcc7491d4011ea557
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2025-05-12 20:00:58 +0100

    refactor: standardise casing of variable/function names

--- libs/Aeron.Driver/java-with-media-driver-classpath.sh
+++ libs/Aeron.Driver/java-with-media-driver-classpath.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 # define constants
-aeronGroup="io.aeron"
-aeronDriverArtifactId="aeron-driver"
-aeronDriverArtifactVersion="1.47.5"
-javaVersion="java-17"
+AERON_GROUP="io.aeron"
+AERON_DRIVER_ARTIFACT_ID="aeron-driver"
+AERON_DRIVER_ARTIFACT_VERSION="1.47.5"
+JAVA_VERSION="17"
 
 # https://stackoverflow.com/a/77663806/11045433
-AeronDriverDirectory=$(dirname "$( readlink -f "${BASH_SOURCE[0]:-"$( command -v -- "$0" )"}" )")
+script_directory=$(dirname "$( readlink -f "${BASH_SOURCE[0]:-"$( command -v -- "$0" )"}" )")
 
 # https://serverfault.com/a/1100799
-Java=$(update-alternatives --list java | grep "$javaVersion")
+java_exe=$(update-alternatives --list java | grep "java-$JAVA_VERSION")
 
-function groupPathFromGroupName() {
-  local groupName="${1:?missing group name argument}"
-  echo "$groupName" | sed 's/\./\//g'
+function GroupPathFromGroupName() {
+  local group="${1:?missing group name argument}"
+  echo "$group" | sed 's/\./\//g'
 }
 
 function GetArtifactFromMaven() {
@@ -30,46 +30,46 @@ function ResolveClasspath() {
   local group="${1:?missing group name argument}"
   local artifact="${2:?missing artifact ID argument}"
   local version="${3:?missing artifact version argument}"
-  local classpathFile="${4:?missing classpath file argument}"
+  local classpath_file="${4:?missing classpath file argument}"
 
-  local groupPath
-  groupPath="$(groupPathFromGroupName "$group")"
-  local artifactFileStem="$HOME/.m2/repository/$groupPath/$artifact/$version/$artifact-$version"
-  local artifactJar="$artifactFileStem".jar
-  local artifactPom="$artifactFileStem".pom
+  local group_path
+  group_path="$(GroupPathFromGroupName "$group")"
+  local artifact_file_stem="$HOME/.m2/repository/$group_path/$artifact/$version/$artifact-$version"
+  local artifact_jar="$artifact_file_stem".jar
+  local artifact_pom="$artifact_file_stem".pom
 
-  if [ ! -e "$artifactJar" ]; then
+  if [ ! -e "$artifact_jar" ]; then
     GetArtifactFromMaven "$group" "$artifact" "$version"
   fi
 
   echo "Building classpath using Maven..." >&2
-  mvn -f "$artifactPom" dependency:build-classpath \
+  mvn -f "$artifact_pom" dependency:build-classpath \
     -Dmdep.includeScope=runtime \
-    -Dmdep.outputFile="$classpathFile" >&2
-  printf ":%s" "$artifactJar" >> "$classpathFile"
+    -Dmdep.outputFile="$classpath_file" >&2
+  printf ":%s" "$artifact_jar" >> "$classpath_file"
 }
 
 function CachedResolveClasspath() {
   local group="${1:?missing group name argument}"
   local artifact="${2:?missing artifact ID argument}"
   local version="${3:?missing artifact version argument}"
-  local classpathFile="${4:?missing classpath file argument}"
+  local classpath_file="${4:?missing classpath file argument}"
 
-  if [ ! -e "$classpathFile" ]; then
-    ResolveClasspath "$group" "$artifact" "$version" "$classpathFile"
+  if [ ! -e "$classpath_file" ]; then
+    ResolveClasspath "$group" "$artifact" "$version" "$classpath_file"
   fi
-  cat "$classpathFile"
+  cat "$classpath_file"
 }
 
 function PrepareMediaDriverClasspath() {
-  mediaDriverClasspath="$(
-    CachedResolveClasspath "$aeronGroup" "$aeronDriverArtifactId" "$aeronDriverArtifactVersion" \
-      "$AeronDriverDirectory/classpath.txt.local"
+  MEDIA_DRIVER_CLASSPATH="$(
+    CachedResolveClasspath "$AERON_GROUP" "$AERON_DRIVER_ARTIFACT_ID" "$AERON_DRIVER_ARTIFACT_VERSION" \
+      "$script_directory/classpath.txt.local"
   )"
 }
 export -f PrepareMediaDriverClasspath
 
 function JavaWithMediaDriverClasspath() {
-  "$Java" --add-exports java.base/jdk.internal.misc=ALL-UNNAMED -cp "$mediaDriverClasspath" "$@"
+  "$java_exe" --add-exports java.base/jdk.internal.misc=ALL-UNNAMED -cp "$MEDIA_DRIVER_CLASSPATH" "$@"
 }
 export -f JavaWithMediaDriverClasspath

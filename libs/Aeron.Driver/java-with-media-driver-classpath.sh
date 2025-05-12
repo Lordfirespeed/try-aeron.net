a5e4b3c9fdcc936a797d42c43d40e99d4a4677c7
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2025-05-12 22:45:44 +0100

    fix: recombine null delimited array into colon-delimited

--- libs/Aeron.Driver/java-with-media-driver-classpath.sh
+++ libs/Aeron.Driver/java-with-media-driver-classpath.sh
@@ -12,15 +12,22 @@ script_directory=$(dirname "$( readlink -f "${BASH_SOURCE[0]:-"$( command -v -- 
 # https://serverfault.com/a/1100799
 java_exe=$(update-alternatives --list java | grep "java-$JAVA_VERSION")
 
+source "$script_directory/delimited-array.sh"
 source "$script_directory/maven-util.sh"
 
 function PrepareJavaWithMediaDriverClasspath() {
-  MEDIA_DRIVER_CLASSPATH="$(
-    ResolveClasspathAndEnsureExists "$AERON_GROUP" "$AERON_DRIVER_ARTIFACT_ID" "$AERON_DRIVER_ARTIFACT_VERSION" \
+  local media_driver_classpath_array
+  Read0 media_driver_classpath_array < <(
+    ResolveClasspathAndEnsureExists \
+      "$AERON_GROUP" \
+      "$AERON_DRIVER_ARTIFACT_ID" \
+      "$AERON_DRIVER_ARTIFACT_VERSION" \
       "$script_directory/classpath.txt.local"
-  )"
+  )
+
+  MEDIA_DRIVER_CLASSPATH="$(PrintSep ":" "${media_driver_classpath_array[@]}")"
 }
-export -f PrepareMediaDriverClasspath
+export -f PrepareJavaWithMediaDriverClasspath
 
 function JavaWithMediaDriverClasspath() {
   "$java_exe" --add-exports java.base/jdk.internal.misc=ALL-UNNAMED -cp "$MEDIA_DRIVER_CLASSPATH" "$@"
